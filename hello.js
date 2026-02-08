44fec6cd0ff2723eff3cf8ad8dd18e441babb0cc
Harsha H G <hahg@redhat.com>
2026-02-08 17:02:20 +0530

    console log and stdout write

---
+++ hello.js
@@ -0,0 +1,4 @@
+console.log("harsh welcome to javascript"); // this will make auto ny default 
+process.stdout.write("welcome js "); // this for printing 
+process.stdout.write("this testing the stdout write ");// this will not print in the new line but console log prints in the new line
+
