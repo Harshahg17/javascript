02fdfe890e6f93281c081f7e881d19fb32bd9752
Harsha H G <hahg@redhat.com>
2026-02-08 18:12:32 +0530

    this the scope of all variable

--- spreadop.js
+++ spreadop.js
@@ -1,8 +1,16 @@
-let obj1={
-    name:"harsha",
-    age: 21
-} 
-obj2=obj1; // obj1 one memory ref and obj2 memory ref both are same here 
-obj2.name="karthik"
-console.log(obj1.name) // ans is karthik because this is the pointing same memory location 
+// let obj1={
+//     name:"harsha",
+//     age: 21
+// } 
+// obj2=obj1; // obj1 one memory ref and obj2 memory ref both are same here 
+// obj2.name="karthik"
+// console.log(obj1.name) // ans is karthik because this is the pointing same memory location 
 
+// // using spread operator we can copy real in different memory location 
+
+// obj3={...obj1}
+// obj3.name="spread"
+// console.log(obj1.name)
+
+
+let obj1
---
+++ var.js
@@ -0,0 +1,6 @@
+// var is function scope
+// let is the block scope 
+// const also block scope
+//var → function scope
+//let → block scope
+//const → block scope + cannot change
